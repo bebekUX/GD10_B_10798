83ea86e17a518e0f803225415a16033e673758af
bebekUX <briantana.869@gmail.com>
2022-11-10 09:34:20 +0700

    GD10_B_10798

---
+++ app/src/main/java/com/example/gd10_b_10798/ResponseCreate.kt
@@ -0,0 +1,9 @@
+package com.example.gd10_b_10798
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseCreate(
+    @SerializedName("status") val stt:Int,
+    @SerializedName("error") val e:Boolean,
+    @SerializedName("message") val pesan:String,
+)
---
+++ app/src/main/java/com/example/gd10_b_10798/ResponseDataMahasiswa.kt
@@ -0,0 +1,11 @@
+package com.example.gd10_b_10798
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>
+)
+
+
